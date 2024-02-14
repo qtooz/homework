1880a85571a89c420413ee0a2ac26504e27bd4a9
qtooz <157675446+qtooz@users.noreply.github.com>
2024-02-14 16:29:19 +0700

    add lesson81

---
+++ lesson81/js/main.js
@@ -0,0 +1,9 @@
+const numbers = [0, 1, 2, 3, 4, 5]
+for(i = 0; i < numbers.length; i++) {
+	if(i % 2 == 0) {console.log(numbers[i] )}
+}
+
+const color = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
+for (i = 6; i >= 0; i = i - 1) {
+	console.log(color[i])
+}
