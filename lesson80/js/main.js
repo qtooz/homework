1970e3082df2b195a4a78d75572f01a909a01a83
qtooz <157675446+qtooz@users.noreply.github.com>
2024-02-14 15:28:19 +0700

    add lesson80

---
+++ lesson80/js/main.js
@@ -0,0 +1,24 @@
+
+for(let i = 1; i <= 12; i = i + 1) {
+	if (i % 2 == 0) {
+	console.log(i)
+}
+}
+
+// let i = 0
+// while (i < 12) {
+// 	i = i + 1
+// 	if (i % 2 == 0) {
+// 		console.log(i)
+// 	}
+// }
+
+// for(let i = 10; i >= 1 ; i = i - 1) {
+// 	console.log(i)
+// }
+
+// let i = 11
+// while (i > 1) {
+// 	i = i - 1
+// 	console.log(i)
+// }
